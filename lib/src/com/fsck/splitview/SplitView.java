2c544d9610dc893c21c90b20e0786e087ace1cc5
Jesse Vincent <jesse@bestpractical.com>
2011-03-02 15:03:02 -0500

    Initial working version

--- lib/src/com/fsck/splitview/SplitView.java
+++ lib/src/com/fsck/splitview/SplitView.java
@@ -6,11 +6,19 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.Log;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+import android.view.ViewGroup;
+import android.view.View.OnTouchListener;
 import android.widget.LinearLayout;
 
-class SplitView extends LinearLayout {
-	
+
+class SplitView extends LinearLayout implements OnTouchListener{
+
+    final float scale = getContext().getResources().getDisplayMetrics().density;
+
+
     private int mHandleId;
 	private View mHandle;
 
@@ -20,7 +28,8 @@ class SplitView extends LinearLayout {
     private int mSecondaryContentId;
     private View mSecondaryContent;
 
-
+    private boolean mDragging;
+    private float mPointerOffset;
 
     public SplitView(Context context, AttributeSet attrs) {
                 super(context, attrs);
@@ -34,8 +43,6 @@ class SplitView extends LinearLayout {
 					": The required attribute handle must refer to a valid child view.");
 		}
 
-
-    
     mPrimaryContentId = viewAttrs.getResourceId(R.styleable.SplitView_primaryContent, 0);
 		if (mPrimaryContentId == 0) {
 		e= new IllegalArgumentException(viewAttrs.getPositionDescription() +
@@ -49,12 +56,80 @@ class SplitView extends LinearLayout {
 					": The required attribute secondaryContent must refer to a valid child view.");
 		}
 
-        
+
+
 		viewAttrs.recycle();
 		
 		if (e != null) {
 			throw e;
 		}
 
+    
+    
     }
+
+    @Override
+    public void onFinishInflate() {
+        super.onFinishInflate();
+        mHandle = findViewById(mHandleId); 
+        if (mHandle == null ) {
+            String name = getResources().getResourceEntryName(mHandleId);
+            throw new RuntimeException("Your Panel must have a child View whose id attribute is 'R.id." + name + "'");
+
+        }
+        mPrimaryContent = findViewById(mPrimaryContentId); 
+        if (mPrimaryContent == null ) {
+            String name = getResources().getResourceEntryName(mPrimaryContentId);
+            throw new RuntimeException("Your Panel must have a child View whose id attribute is 'R.id." + name + "'");
+
+        }
+        mSecondaryContent = findViewById(mSecondaryContentId); 
+        if (mSecondaryContent == null ) {
+            String name = getResources().getResourceEntryName(mSecondaryContentId);
+            throw new RuntimeException("Your Panel must have a child View whose id attribute is 'R.id." + name + "'");
+
+        }
+        
+        mHandle.setOnTouchListener(this);
+        setOnTouchListener(this);
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent me) {
+         
+         ViewGroup.LayoutParams thisParams = getLayoutParams();
+        // Only capture drag events if we start 
+        if (view != mHandle ) {
+            return false;
+        }
+
+        if (me.getAction() == MotionEvent.ACTION_DOWN) {
+            mDragging = true;
+            if (getOrientation() == VERTICAL) {
+            mPointerOffset = me.getRawY() - mPrimaryContent.getMeasuredHeight();
+            } else {
+            mPointerOffset = me.getRawX() - mPrimaryContent.getMeasuredWidth();
+            }
+        }
+        if (me.getAction() == MotionEvent.ACTION_UP) {
+            mDragging = false;
+        } else if (me.getAction() == MotionEvent.ACTION_MOVE) {
+            ViewGroup.LayoutParams params = mPrimaryContent.getLayoutParams();
+            if (getOrientation() == VERTICAL) {
+                    int newHeight =  (int) ((scale * me.getRawY()) - mPointerOffset);
+                params.height = newHeight;
+            } else {
+                int newWidth =  (int) ((scale * me.getRawX()) - mPointerOffset);
+                params.width = newWidth;
+            }   
+            mPrimaryContent.setLayoutParams(params);
+
+
+
+        }
+
+        return true;
+    }
+
 };
