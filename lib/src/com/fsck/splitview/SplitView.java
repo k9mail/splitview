457e4804fba972d6d84eb77b62a0233896027d99
Jesse Vincent <jesse@bestpractical.com>
2011-03-06 18:53:36 -0500

    astyle

--- lib/src/com/fsck/splitview/SplitView.java
+++ lib/src/com/fsck/splitview/SplitView.java
@@ -13,11 +13,10 @@ import android.view.ViewGroup;
 import android.view.View.OnTouchListener;
 import android.widget.LinearLayout;
 
-
 class SplitView extends LinearLayout implements OnTouchListener {
 
     private int mHandleId;
-	private View mHandle;
+    private View mHandle;
 
     private int mPrimaryContentId;
     private View mPrimaryContent;
@@ -29,64 +28,64 @@ class SplitView extends LinearLayout implements OnTouchListener {
     private float mPointerOffset;
 
     public SplitView(Context context, AttributeSet attrs) {
-                super(context, attrs);
-	
-		TypedArray viewAttrs = context.obtainStyledAttributes(attrs, R.styleable.SplitView);
-    
-		RuntimeException e = null;
-    mHandleId = viewAttrs.getResourceId(R.styleable.SplitView_handle, 0);
-		if (mHandleId == 0) {
-		e= new IllegalArgumentException(viewAttrs.getPositionDescription() +
-					": The required attribute handle must refer to a valid child view.");
-		}
+        super(context, attrs);
 
-    mPrimaryContentId = viewAttrs.getResourceId(R.styleable.SplitView_primaryContent, 0);
-		if (mPrimaryContentId == 0) {
-		e= new IllegalArgumentException(viewAttrs.getPositionDescription() +
-					": The required attribute primaryContent must refer to a valid child view.");
-		}
+        TypedArray viewAttrs = context.obtainStyledAttributes(attrs, R.styleable.SplitView);
 
-    
-    mSecondaryContentId = viewAttrs.getResourceId(R.styleable.SplitView_secondaryContent, 0);
-		if (mSecondaryContentId == 0) {
-	    e = new IllegalArgumentException(viewAttrs.getPositionDescription() +
-					": The required attribute secondaryContent must refer to a valid child view.");
-		}
+        RuntimeException e = null;
+        mHandleId = viewAttrs.getResourceId(R.styleable.SplitView_handle, 0);
+        if (mHandleId == 0) {
+            e = new IllegalArgumentException(viewAttrs.getPositionDescription() +
+                                             ": The required attribute handle must refer to a valid child view.");
+        }
+
+        mPrimaryContentId = viewAttrs.getResourceId(R.styleable.SplitView_primaryContent, 0);
+        if (mPrimaryContentId == 0) {
+            e = new IllegalArgumentException(viewAttrs.getPositionDescription() +
+                                             ": The required attribute primaryContent must refer to a valid child view.");
+        }
 
 
+        mSecondaryContentId = viewAttrs.getResourceId(R.styleable.SplitView_secondaryContent, 0);
+        if (mSecondaryContentId == 0) {
+            e = new IllegalArgumentException(viewAttrs.getPositionDescription() +
+                                             ": The required attribute secondaryContent must refer to a valid child view.");
+        }
 
-		viewAttrs.recycle();
-		
-		if (e != null) {
-			throw e;
-		}
 
-    
-    
+
+        viewAttrs.recycle();
+
+        if (e != null) {
+            throw e;
+        }
+
+
+
     }
 
     @Override
     public void onFinishInflate() {
         super.onFinishInflate();
-        mHandle = findViewById(mHandleId); 
-        if (mHandle == null ) {
+        mHandle = findViewById(mHandleId);
+        if (mHandle == null) {
             String name = getResources().getResourceEntryName(mHandleId);
             throw new RuntimeException("Your Panel must have a child View whose id attribute is 'R.id." + name + "'");
 
         }
-        mPrimaryContent = findViewById(mPrimaryContentId); 
-        if (mPrimaryContent == null ) {
+        mPrimaryContent = findViewById(mPrimaryContentId);
+        if (mPrimaryContent == null) {
             String name = getResources().getResourceEntryName(mPrimaryContentId);
             throw new RuntimeException("Your Panel must have a child View whose id attribute is 'R.id." + name + "'");
 
         }
-        mSecondaryContent = findViewById(mSecondaryContentId); 
-        if (mSecondaryContent == null ) {
+        mSecondaryContent = findViewById(mSecondaryContentId);
+        if (mSecondaryContent == null) {
             String name = getResources().getResourceEntryName(mSecondaryContentId);
             throw new RuntimeException("Your Panel must have a child View whose id attribute is 'R.id." + name + "'");
 
         }
-        
+
         mHandle.setOnTouchListener(this);
         setOnTouchListener(this);
 
@@ -94,19 +93,19 @@ class SplitView extends LinearLayout implements OnTouchListener {
 
     @Override
     public boolean onTouch(View view, MotionEvent me) {
-         
-         ViewGroup.LayoutParams thisParams = getLayoutParams();
-        // Only capture drag events if we start 
-        if (view != mHandle ) {
+
+        ViewGroup.LayoutParams thisParams = getLayoutParams();
+        // Only capture drag events if we start
+        if (view != mHandle) {
             return false;
         }
 
         if (me.getAction() == MotionEvent.ACTION_DOWN) {
             mDragging = true;
             if (getOrientation() == VERTICAL) {
-            mPointerOffset = me.getRawY() - mPrimaryContent.getMeasuredHeight();
+                mPointerOffset = me.getRawY() - mPrimaryContent.getMeasuredHeight();
             } else {
-            mPointerOffset = me.getRawX() - mPrimaryContent.getMeasuredWidth();
+                mPointerOffset = me.getRawX() - mPrimaryContent.getMeasuredWidth();
             }
         }
         if (me.getAction() == MotionEvent.ACTION_UP) {
@@ -114,18 +113,22 @@ class SplitView extends LinearLayout implements OnTouchListener {
         } else if (me.getAction() == MotionEvent.ACTION_MOVE) {
             ViewGroup.LayoutParams params = mPrimaryContent.getLayoutParams();
             if (getOrientation() == VERTICAL) {
-                    int newHeight =  (int) ( me.getRawY() - mPointerOffset);
-                if (mSecondaryContent.getMeasuredHeight() < 1 && newHeight > params.height) { return false;}
-                    if (newHeight >= 0) {
-                params.height = newHeight;
-                    }
+                int newHeight = (int)(me.getRawY() - mPointerOffset);
+                if (mSecondaryContent.getMeasuredHeight() < 1 && newHeight > params.height) {
+                    return false;
+                }
+                if (newHeight >= 0) {
+                    params.height = newHeight;
+                }
             } else {
-                int newWidth =  (int) (me.getRawX() - mPointerOffset);
-                if (mSecondaryContent.getMeasuredWidth() < 1 && newWidth > params.width) { return false;}
-                if (newWidth >= 0  ) {
+                int newWidth = (int)(me.getRawX() - mPointerOffset);
+                if (mSecondaryContent.getMeasuredWidth() < 1 && newWidth > params.width) {
+                    return false;
+                }
+                if (newWidth >= 0) {
                     params.width = newWidth;
                 }
-            }   
+            }
             mPrimaryContent.setLayoutParams(params);
 
 
